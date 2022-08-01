965e6ef47ba314f7af7e9a68a59cd75ce5b55ab4
ratchek <git@ratchek.com>
2022-08-01 13:06:35 -0400

    Basic setup

---
+++ src/game.py
@@ -0,0 +1,36 @@
+# Pygame template - skeleton for a new pygame project
+import pygame
+import random
+import settings
+
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
+pygame.display.set_caption(settings.TITLE)
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+
+
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(settings.FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+    # Update
+    all_sprites.update()
+    # Draw / render
+    screen.fill(settings.BLACK)
+    all_sprites.draw(screen)
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
---
+++ src/settings.py
@@ -0,0 +1,12 @@
+WIDTH = 720
+HEIGHT = 480
+FPS = 60
+TITLE = "Bubble Trouble"
+
+# define colors. Used for development
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
